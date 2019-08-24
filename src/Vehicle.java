37300eab2cd950fb64adbaf0f67dda7e8b9b3ed1
adria <Konfitury2>
2019-08-24 18:25:29 +0200

    Dodanie klas

---
+++ src/Vehicle.java
@@ -0,0 +1,80 @@
+public class Vehicle {
+    private String type;
+    private String brand;
+    private String model;
+    private int year;
+    private int mileage;
+    private String VIN;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String type, String brand, String model, int year, int mileage, String VIN) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.VIN = VIN;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", mileage=" + mileage +
+                ", VIN='" + VIN + '\'' +
+                '}';
+    }
+}
